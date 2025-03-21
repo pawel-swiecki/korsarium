167fae12d90abda52878c5b798f921e68951e1b7
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-03-21 14:07:11 -0400

    Initialize website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "pages#main"
+
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
