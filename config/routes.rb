6dd6fca029035f89beb5ffa7f80e2ca4d921fac1
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-03-24 11:50:39 -0400

    Rearrange routes in alphabetical order
    Alphabetical order of the controllers should help with locating which
    route you want to edit faster.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "pages#main"
 
-  resource :session
-  resource :registration, only: %i[new create]
-
   resources :passwords, param: :token
+  resource :registration, only: %i[new create]
+  resource :session
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
