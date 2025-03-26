e59c00e823456616c3377333ca8135d3653a0f32
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-03-26 09:08:23 -0400

    Initialize CRUD functionality for Courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,20 +1,48 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: %i[ show edit update destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @courses = Course.all
   end
 
   def show
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @course.update(course_params)
+      redirect_to @course
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to courses_path
+  end
+
+  private
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.expect(course: [ :name ])
+  end
 end
