c63c8b8ce7e5e788606e210b8948d4bdc561f42a
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-08-14 11:36:24 -0400

    Create segments#show

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -3,4 +3,8 @@ class SegmentsController < ApplicationController
     @segments = Segment.all
     @selected_segment = Segment.find_by(id: params[:segment_id]) || @segments.first
   end
+
+  def show
+    @segment = Segment.find(params[:id])
+  end
 end
