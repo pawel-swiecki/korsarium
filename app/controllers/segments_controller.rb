51d1eb081effed3b2c11d6252588bf59297da3c8
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-08-14 11:12:38 -0400

    Create segments#index

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,2 +1,6 @@
 class SegmentsController < ApplicationController
+  def index
+    @segments = Segment.all
+    @selected_segment = Segment.find_by(id: params[:segment_id]) || @segments.first
+  end
 end
