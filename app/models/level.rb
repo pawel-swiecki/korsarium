a817e46f82e2fc82c85f216d98b2ba02bd8fabfe
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-22 13:48:29 -0400

    Delete old levels and segments

--- app/models/level.rb
+++
@@ -1,4 +0,0 @@
-class Level < ApplicationRecord
-  has_many :segments, dependent: :destroy
-  belongs_to :course, optional: true
-end
--- app/models/segment.rb
+++
@@ -1,4 +0,0 @@
-class Segment < ApplicationRecord
-  has_many :lessons, dependent: :destroy
-  belongs_to :level, optional: true
-end
