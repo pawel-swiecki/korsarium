6ea2efdc65ea0f63c7b1ab7f440d77712c929157
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-22 14:52:30 -0400

    Generate controller segments

---
+++ app/controllers/segments_controller.rb
@@ -0,0 +1,7 @@
+class SegmentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- test/controllers/segments_controller_test.rb
+++ test/controllers/segments_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class SegmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get segments_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get segments_show_url
+    assert_response :success
+  end
 end
