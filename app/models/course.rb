9a4eacb3f6a9ebf6aafa85f5e23f7b26393b14db
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-23 08:41:33 -0400

    Associate course with segments

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   has_one_attached :course_icon
-  has_many :levels, dependent: :destroy
+  has_many :segments, dependent: :destroy
 
   validates :title, presence: true
   validates :subtitle, presence: true
