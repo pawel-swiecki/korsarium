df0a24fc852295075992411cccd97a15a4555ce0
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-29 13:58:55 -0400

    Create segments#edit and set_segment

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,6 +1,16 @@
 class SegmentsController < ApplicationController
+  before_action :set_segment, only: %i[show edit]
+
   def show
-    @segment = Segment.find(params[:id])
     @levels = @segment.levels
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_segment
+    @segment = Segment.find(params[:id])
   end
 end
