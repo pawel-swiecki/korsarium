e65cc42ebb52c646cf47da5710849a5b5a2b4d11
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-29 14:55:16 -0400

    Create segments#edit and update

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,5 +1,5 @@
 class SegmentsController < ApplicationController
-  before_action :set_segment, only: %i[show edit]
+  before_action :set_segment, only: %i[show edit update]
 
   def show
     @levels = @segment.levels
@@ -8,9 +8,21 @@ class SegmentsController < ApplicationController
   def edit
   end
 
+  def update
+    if @segment.update(segment_params)
+      redirect_to @segment
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_segment
     @segment = Segment.find(params[:id])
+  end
+
+  def segment_params
+    params.expect(segment: [ :title, :segment_icon ])
   end
 end
