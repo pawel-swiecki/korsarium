2e315eae32449bf4820ee62b14cc806f86946f44
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-30 11:30:16 -0400

    Migrate AddSubtitleToSegment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_28_160045) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_30_152551) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_28_160045) do
     t.integer "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subtitle"
     t.index ["course_id"], name: "index_segments_on_course_id"
   end
 
