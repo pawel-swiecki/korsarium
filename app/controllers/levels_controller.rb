0f8906fafa88ee562b75b3bebe143d345fd9bf93
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-31 11:24:55 -0400

    Create levels# set_level, level_params, edit, update

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,6 +1,28 @@
 class LevelsController < ApplicationController
+  before_action :set_level, only: %i[ show edit update]
+
   def show
-    @level = Level.find(params[:id])
     @textbook = @level.textbook
+  end
+
+  def edit
+  end
+
+  def update
+    if @level.update(level_params)
+      redirect_to @level
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_level
+    @level = Level.find(params[:id])
+  end
+
+  def level_params
+    params.expect(level: [ :level_icon, :title, :subtitle ])
   end
 end
