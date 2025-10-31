be98f11d6e8016d758ad6ac1060ae874effd8899
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-10-31 11:28:02 -0400

    Migrate AddSubtitleToLevel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_30_152551) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_31_152605) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_30_152551) do
     t.integer "segment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subtitle"
     t.index ["segment_id"], name: "index_levels_on_segment_id"
   end
 
