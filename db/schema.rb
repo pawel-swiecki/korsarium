45b81b5d995f05d0160b51f4b348dac0e3594ea2
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-11-17 14:58:03 -0500

    Migrate add_names_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_17_145001) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_17_195652) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -97,6 +97,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_17_145001) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
