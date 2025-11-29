7db8e4602e176858593af9ea5983af37dad52d37
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-11-29 12:30:41 -0500

    Generate mailer User

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.email_confirmation.subject
+  #
+  def email_confirmation
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
