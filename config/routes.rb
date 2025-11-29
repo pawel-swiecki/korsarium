de3cbfbcaa6648386bb1356ca4f38d6c88b91fa1
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-11-29 14:10:18 -0500

    Create routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
     resource :email, only: [ :show, :update ]
     resource :password, only: [ :show, :update ]
     resource :profile, only: [ :show, :update ]
+    resource :user, only: [ :show, :destroy ]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
