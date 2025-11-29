c11afca1eb9c224cd9aacbd52452fe496606656f
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-11-29 12:21:32 -0500

    Migrate AddUnconfirmedEmailToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_17_195652) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_29_172029) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -100,6 +100,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_17_195652) do
     t.string "first_name"
     t.string "last_name"
     t.string "password_digest", null: false
+    t.string "unconfirmed_email"
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
