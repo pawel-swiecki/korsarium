982fdf152b4e03fb6fd9941f40602994e409394f
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-11-30 12:03:28 -0500

    Redirect to courses#index after authentication

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -40,7 +40,7 @@ module Authentication
     end
 
     def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
+      session.delete(:return_to_after_authenticating) || courses_url
     end
 
     def start_new_session_for(user)
