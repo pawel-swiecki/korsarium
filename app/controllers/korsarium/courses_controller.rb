c4adc70bcb2f13e1ddd771163fb4f2610a2b9985
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-11-30 11:43:25 -0500

    Create korsarium/courses controller

---
+++ app/controllers/korsarium/courses_controller.rb
@@ -0,0 +1,48 @@
+class Korsarium::ProductsController < Korsarium::BaseController
+  before_action :set_course, only: %i[ show edit update destroy ]
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to korsarium_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to korsarium_course_path(@course)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to korsarium_courses_path
+  end
+
+  private
+    def set_course
+      @course = Course.find(params[:id])
+    end
+
+    def course_params
+      params.expect(course: [ :title, :subtitle, :course_icon ])
+    end
+end
