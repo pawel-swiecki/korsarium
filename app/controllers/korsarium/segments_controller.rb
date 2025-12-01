68404296e2d1c678e864a09e4d586de0508fc6ae
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-01 09:35:24 -0500

    Create korsarium/segments controller

--- app/controllers/korsarium/segments_controller.rb
+++ app/controllers/korsarium/segments_controller.rb
@@ -1,1 +1,45 @@
 class Korsarium::SegmentsController < Korsarium::BaseController
+  before_action :set_segment, only: %i[ show edit update destroy ]
+
+  def index
+    @segments = Segment.all
+  end
+
+  def show
+    @levels = @segment.levels
+  end
+
+  def new
+    @segment = Segment.new
+  end
+
+  def create
+    @segment = Segment.new(segment_params)
+    if @segment.save
+      redirect_to korsarium_segment_path(@segment)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @segment.update(segment_params)
+      redirect_to korsarium_segment_path(@segment)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_segment
+    @segment = Segment.find(params[:id])
+  end
+
+  def segment_params
+    params.expect(segment: [ :segment_icon, :title, :subtitle ])
+  end
+end
