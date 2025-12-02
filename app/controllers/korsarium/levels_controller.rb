4066b49136e052816182a0b0a415cd90915996f3
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-02 10:18:46 -0500

    Create korsarium::levels controller

---
+++ app/controllers/korsarium/levels_controller.rb
@@ -0,0 +1,40 @@
+class Korsarium::LevelsController < Korsarium::BaseController
+  before_action :set_level, only: %i[ show edit update]
+
+  def index
+  end
+
+  def show
+    @textbook = @level.textbook
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+    if @level.update(level_params)
+      redirect_to @level
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_level
+    @level = Level.find(params[:id])
+  end
+
+  def level_params
+    params.expect(level: [ :level_icon, :title, :subtitle ])
+  end
+end
