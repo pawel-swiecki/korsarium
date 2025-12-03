79b9621807eb895d31f4c04fcb1017991c6a9422
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-03 08:22:01 -0500

    Create CRUD actions for korsarium::lesson controller

--- app/controllers/korsarium/lessons_controller.rb
+++ app/controllers/korsarium/lessons_controller.rb
@@ -1,22 +1,55 @@
 class Korsarium::LessonsController < Korsarium::BaseController
+  before_action :set_lesson, only: %i[ show edit update destroy ]
+  before_action :set_textbook, only: %i[ new create ]
+
   def index
+    @lessons = Lesson.all
   end
 
   def show
   end
 
   def new
+    @lesson = Lesson.new
   end
 
   def create
+    @lesson = @textbook.lessons.build(lesson_params)
+    if @lesson.save
+      redirect_to @lesson
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @lesson.update(lesson_params)
+      redirect_to @lesson
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @textbook = @lesson.textbook_id
+    @lesson.destroy
+    redirect_to @textbook
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def set_textbook
+    @textbook = Textbook.find(params[:textbook_id])
+  end
+
+  def lesson_params
+    params.expect(lesson: [ :title, :body, :introduction, :summary ])
   end
 end
