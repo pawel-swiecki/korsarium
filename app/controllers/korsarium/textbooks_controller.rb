32f68fc76484e40b4349cb5b314e112d7a91ef58
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-03 08:13:34 -0500

    Create korsarium::textbooks controller

---
+++ app/controllers/korsarium/textbooks_controller.rb
@@ -0,0 +1,23 @@
+class Korsarium::TextbooksController < Korsarium::BaseController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
