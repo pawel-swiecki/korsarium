724435007bd332428dc765b6824f7fd9b1917efb
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-03 09:30:51 -0500

    Create CRUD actions for korsarium::textbooks

--- app/controllers/korsarium/textbooks_controller.rb
+++ app/controllers/korsarium/textbooks_controller.rb
@@ -1,23 +1,56 @@
 class Korsarium::TextbooksController < Korsarium::BaseController
+  before_action :set_textbook, only: %i[ show edit update destroy ]
+  before_action :set_level, only: %i[ new create ]
+
   def index
+    @textbooks = Textbook.all
   end
 
   def show
   end
 
   def new
+    @textbook = Textbook.new
   end
 
   def create
+    @textbook = @level.textbooks.build(textbook_params)
+    if @textbook.save
+      redirect_to @textbook
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @textbook.update(textbook_params)
+      redirect_to @textbook
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @level = @textbook.level_id
+    @textbook.destroy
+    redirect_to @level
+  end
+
+  private
+
+  def set_textbook
+    @textbook = Textbook.find(params[:id])
+  end
+
+  def set_level
+    @level = Level.find(params[:level_id])
+  end
+
+  def textbook_params
+    params.expect(textbook: [ :textbook_icon, :title, :subtitle ])
   end
 end
 
