655295adb929ffadb6821a58538e38b705096f2d
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-03 08:13:27 -0500

    Create korsarium::lessons controller

---
+++ app/controllers/korsarium/lessons_controller.rb
@@ -0,0 +1,22 @@
+class Korsarium::LessonsController < Korsarium::BaseController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
